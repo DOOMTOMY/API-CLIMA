2b4bb9ef06074127926fd28d878cad3f13e80925
DOOMTOMY <Thiago.tomczykt@outlook.com>
2024-07-31 10:40:33 -0300

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,100 @@
+const apiKey = "e1865ec956a97708bff69b0a79e8617b";
+const apiCountryURL = "https://flagsapi.com/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+const weatherContainer = document.querySelector("#weather-data");
+const errorMessageContainer = document.querySelector("#error-message");
+const loader = document.querySelector("#loader");
+const suggestionContainer = document.querySelector("#suggestions");
+const suggestionButtons = document.querySelectorAll("#suggestions button");
+const cleanButton = document.getElementById('clean');
+
+const toggleLoader = () => {
+  loader.classList.toggle("hide");
+};
+
+async function getWeatherData(city) {
+    toggleLoader();
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    toggleLoader();
+
+    return data;
+}
+
+const showErrorMessage = () => {
+  errorMessageContainer.classList.remove("hide");
+};
+
+const hideInformation = () => {
+  errorMessageContainer.classList.add("hide");
+  weatherContainer.classList.add("hide");
+  suggestionContainer.classList.add("hide");
+};
+
+const showWeatherData = async (city) => {
+  hideInformation();
+
+  const data = await getWeatherData(city);
+
+  if (data.cod === "404") {
+    showErrorMessage();
+    return;
+  }
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+  );
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country + "/flat/64.png");
+
+  umidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  document.body.style.backgroundImage = `url("https://1.bp.blogspot.com/-2eVGC-Y_ORE/XnzHf3tog4I/AAAAAAAApbM/OrgkdX-QWRUcW0j1EXnEORYXFp6SrK0XgCLcBGAsYHQ/s2560/mountains_forest_lake_164679_2560x1440.jpg")`;
+
+  weatherContainer.classList.remove("hide");
+};
+
+searchBtn.addEventListener("click", async (e) => {
+  e.preventDefault();
+
+  const city = cityInput.value;
+
+  showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+  if (e.code === "Enter") {
+    const city = e.target.value;
+
+    showWeatherData(city);
+  }
+});
+
+suggestionButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const city = btn.getAttribute("id");
+    showWeatherData(city);
+  });
+});
+
+cleanButton.addEventListener('click', () => {
+  cityInput.value = ''; 
+  hideInformation();
+});
